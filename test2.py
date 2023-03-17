10f21ceb22aff1b8469c8d4bc074cd0695d11f15
Eliza Wielocha <61736185+ElizaWielocha@users.noreply.github.com>
2023-03-17 19:48:29 +0100

    added test file2 but it requires conda pygraphviz

---
+++ test2.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import pygraphviz
+
+g1 = nx.MultiGraph(directed=True)
+
+node1 = 'a'
+node2 = 'b'
+
+g1.add_edge(node1,node2,key=1)
+g1.add_edge(node2,node1,key=2)
+
+A = nx.to_agraph(g1)
+A.add_subgraph()
+
+
+plt.plot()
+nx.draw(A, prog='dot')
+#nx.draw(g1, with_labels=True, arrows = True, connectionstyle='arc3, rad = 0.1')
+plt.show()
