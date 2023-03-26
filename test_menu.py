58016985f7dba82cd003cc9214f17ee93458dedb
Eliza Wielocha <61736185+ElizaWielocha@users.noreply.github.com>
2023-03-26 23:36:31 +0200

    added test functions for buttons

--- test_menu.py
+++ test_menu.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter.filedialog import askopenfilename
 
 window = Tk()                             # setup the application object
 # This will create a top-level window (root) having a frame with a title bar, 
@@ -26,29 +27,39 @@ Attributes:
 '''
 
 
-w = Canvas(window, width=250, height=200)
-w.create_rectangle(10, 10, 100, 100, fill="", outline = 'blue')
+w = Canvas(window, width=471, height=351, highlightthickness=0)
+w.configure(bg='black')
+w.create_rectangle(5, 100, 470, 350, fill="", outline = 'white')
 w.pack()
 
 # LABEL
-lbl_welcome = Label(window, text = "Welcome to algorithm Fleury program!", fg='red', font=("Helvetica", 16))
+lbl_welcome = Label(window, text = "Welcome to algorithm Fleury program!", fg='yellow', font=("Helvetica", 20), bg='black')
 lbl_welcome.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-lbl_menu = Label(window, text = "MENU", fg='red', font=("Helvetica", 16))
-lbl_menu.place(relx=0.05, rely=0.35, anchor=W)
+lbl_menu = Label(window, text = "MENU", fg='green', font=("Helvetica", 18, "bold"), bg='black')
+lbl_menu.place(relx=0.5, rely=0.35, anchor=CENTER)
 
 
-lbl_cr_graph = Label(window, text = "Create your own graph", fg = 'blue', font=("Helvetica", 16))
-lbl_cr_graph.place(relx = 0.05, rely=0.5, anchor=W)
+lbl_cr_graph = Label(window, text = "Create your own graph", fg = 'white', font=("Helvetica", 15), bg='black')
+lbl_cr_graph.place(relx = 0.1, rely=0.55, anchor=W)
 
-lbl_load_graph = Label(window, text = "Load your graph from a text file", fg = 'blue', font=("Helvetica", 16))
-lbl_load_graph.place(relx = 0.05, rely=0.6, anchor=W)
+lbl_load_graph = Label(window, text = "Load your graph from a text file", fg = 'white', font=("Helvetica", 15), bg='black')
+lbl_load_graph.place(relx = 0.1, rely=0.67, anchor=W)
 
-btn_cr_graph = Button(window, text = " Start! ", fg = 'blue', font=("Helvetica", 12))
-btn_cr_graph.place(relx = 0.8, rely=0.5, anchor=W)
 
-btn_load_graph = Button(window, text = " Start! ", fg = 'blue', font=("Helvetica", 12))
-btn_load_graph.place(relx = 0.8, rely=0.6, anchor=W)
+def load_graph_clicked():
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename(filetypes=[("Text files", "*.txt")]) # show an "Open" dialog box and return the path to the selected file
+    window.destroy()
+
+def create_graph_clicked():
+    print("Working")
+
+btn_cr_graph = Button(window, text = "  Start  ", fg = 'black', font=("Helvetica", 13), command=create_graph_clicked)
+btn_cr_graph.place(relx = 0.75, rely=0.55, anchor=W)
+
+btn_load_graph = Button(window, text = "  Start  ", fg = 'black', font=("Helvetica", 13), command=load_graph_clicked)
+btn_load_graph.place(relx = 0.75, rely=0.67, anchor=W)
 
 
 
@@ -62,6 +73,7 @@ btn_load_graph.place(relx = 0.8, rely=0.6, anchor=W)
 
 window.title('Fleury Algorithm')              # title of the window
 window.geometry("500x400+10+10")          # width * height + XPOS + YPOS
+window.configure(bg='black')
 window.mainloop()  
 
 
