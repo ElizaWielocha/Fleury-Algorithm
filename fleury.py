f5a8d277456e52e6dee2b29994b2de88c9ba8e37
Eliza Wielocha <61736185+ElizaWielocha@users.noreply.github.com>
2023-04-27 22:34:53 +0200

    cosmetic changes

--- fleury.py
+++ fleury.py
@@ -1,10 +1,10 @@
+# Modules
 import matplotlib.pyplot as plt
 import networkx as nx
 
 from math import pi
 import matplotlib.pyplot as plt
 import numpy as np
-#import time
 
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
@@ -13,8 +13,9 @@ from tkinter import *
 from collections import defaultdict
 import keyboard
 
+# -------------------------------------------------------------------------------------------------
 
-# START MENU 
+# START MENU - Window
 window = Tk()   
 finalList = []
 filename = "" 
@@ -24,28 +25,28 @@ isDirected = False
 loadingGraph = False
 
 
-lbl_line1 = Label(window, text = "_"*100, fg='white', font=("Helvetica", 18, "bold"), bg='black')
+lbl_line1 = Label(window, text = "_"*100, fg='grey', font=("Helvetica", 18, "bold"), bg='white')
 lbl_line1.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-lbl_line2 = Label(window, text = "_"*100, fg='white', font=("Helvetica", 18, "bold"), bg='black')
+lbl_line2 = Label(window, text = "_"*100, fg='grey', font=("Helvetica", 18, "bold"), bg='white')
 lbl_line2.place(relx=0.5, rely=0.8, anchor=CENTER)
 
 # LABEL
-lbl_welcome = Label(window, text = "Welcome to algorithm Fleury program!", fg='yellow', font=("Helvetica", 23), bg='black')
+lbl_welcome = Label(window, text = "Welcome to algorithm Fleury program!", fg='#2B45EC', font=("Helvetica", 23), bg='white')
 lbl_welcome.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-lbl_menu = Label(window, text = "MENU", fg='green', font=("Helvetica", 20, "bold"), bg='black')
+lbl_menu = Label(window, text = "MENU", fg='#419BE9', font=("Helvetica", 20, "bold"), bg='white')
 lbl_menu.place(relx=0.5, rely=0.35, anchor=CENTER)
 
 
-lbl_cr_graph = Label(window, text = "Create your own graph", fg = 'white', font=("Helvetica", 18), bg='black')
+lbl_cr_graph = Label(window, text = "Create your own graph", fg = '#56575A', font=("Helvetica", 18), bg='white')
 lbl_cr_graph.place(relx = 0.1, rely=0.55, anchor=W)
 
-lbl_load_graph = Label(window, text = "Load your graph from a text file", fg = 'white', font=("Helvetica", 18), bg='black')
+lbl_load_graph = Label(window, text = "Load your graph from a text file", fg = '#56575A', font=("Helvetica", 18), bg='white')
 lbl_load_graph.place(relx = 0.1, rely=0.67, anchor=W)
 
 
-# zaladuj graf
+# load graph option
 def load_graph_clicked():
     global loadingGraph
     global filename
@@ -54,21 +55,24 @@ def load_graph_clicked():
     window.destroy()
 
 
-# Stworz graf - nowe okno
+# create graph option - new window
 def create_graph_clicked():
     window.destroy()
     
     window1 = Tk()
 
     # vertices
-    lbl_desc_v = Label(window1, text = "Type number of vertices in the graph: ", fg='yellow', font=("Helvetica", 16, "bold"), bg='black')
-    lbl_desc_v.place(relx=0.1, rely=0.1, anchor=W)
+    lbl_desc_v = Label(window1, text = "Type number of vertices in the graph: ", fg='#2B45EC', font=("Helvetica", 16, "bold"), bg='white')
+    lbl_desc_v.place(relx=0.1, rely=0.08, anchor=W)
+    
+    lbl_desc_vScheme = Label(window1, text = "(vertex numbers begin with 0) ", fg='#56575A', font=("Helvetica", 16), bg='white')
+    lbl_desc_vScheme.place(relx=0.1, rely=0.13, anchor=W)
 
-    ntr_vertices = Entry(window1, width = 5, font=("Helvetica", 20))
-    ntr_vertices.place(relx=0.1, rely=0.17, anchor=W)
+    ntr_vertices = Entry(window1, width = 5, bd=4, font=("Helvetica", 20))
+    ntr_vertices.place(relx=0.1, rely=0.2, anchor=W)
 
     # is it directed?
-    lbl_desc_v = Label(window1, text = "Mark what kind of graph it should be: ", fg='yellow', font=("Helvetica", 16, "bold"), bg='black')
+    lbl_desc_v = Label(window1, text = "Mark what kind of graph it should be: ", fg='#2B45EC', font=("Helvetica", 16, "bold"), bg='white')
     lbl_desc_v.place(relx=0.1, rely=0.3, anchor=W)
 
     def check_if_directed():
@@ -80,20 +84,20 @@ def create_graph_clicked():
             
     choice = IntVar()
     choice.set(2)
-    directed = Radiobutton(window1, text="Directed", variable= choice, value= 1, fg='white', font=("Helvetica", 14), bg='black', selectcolor='black', command = check_if_directed )
+    directed = Radiobutton(window1, text="Directed", variable= choice, value= 1, fg='#56575A', font=("Helvetica", 14), bg='white', selectcolor='white', command = check_if_directed )
     directed.place(relx=0.1, rely=0.37, anchor=W)
-    not_directed = Radiobutton(window1, text="Undirected", variable= choice, value= 2, fg='white', font=("Helvetica", 14), bg='black', selectcolor='black', command = check_if_directed)
+    not_directed = Radiobutton(window1, text="Undirected", variable= choice, value= 2, fg='#56575A', font=("Helvetica", 14), bg='white', selectcolor='white', command = check_if_directed)
     not_directed.place(relx=0.3, rely=0.37, anchor=W)
 
 
     # edges
-    lbl_desc = Label(window1, text = "Type edge vertices and confirm by clicking add button ", fg='yellow', font=("Helvetica", 16, "bold"), bg='black')
+    lbl_desc = Label(window1, text = "Type edge vertices and confirm by clicking add button ", fg='#2B45EC', font=("Helvetica", 16, "bold"), bg='white')
     lbl_desc.place(relx=0.1, rely=0.5, anchor=W)
         
-    lbl_scheme = Label(window1, text = "Scheme: v1 v2 ", fg='white', font=("Helvetica", 16), bg='black')
+    lbl_scheme = Label(window1, text = "Scheme: v1[space]v2 ", fg='#56575A', font=("Helvetica", 16), bg='white')
     lbl_scheme.place(relx=0.1, rely=0.56, anchor=W)
 
-    ntr_edge = Entry(window1, width = 20, font=("Helvetica", 20))
+    ntr_edge = Entry(window1, width = 20, bd=4, font=("Helvetica", 20))
     ntr_edge.place(relx=0.1, rely=0.62, anchor=W)
 
     def clicked_add_edge():
@@ -101,7 +105,7 @@ def create_graph_clicked():
         edges_list.append(tuple(list(map(int, ntr_edge.get().split() )))) # (1, 2) przedzielona spacja z prawej strony
         ntr_edge.delete(0, END)
         
-    btn_add = Button(window1, text = "  Add ", fg = 'green', font=("Helvetica", 13, "bold"), padx=3, pady=3, activebackground='grey', command=clicked_add_edge)
+    btn_add = Button(window1, text = "  Add ", fg = 'black', font=("Helvetica", 13, "bold"), padx=3, pady=3, activebackground='grey', command=clicked_add_edge)
     btn_add.place(relx = 0.58, rely=0.62, anchor=W)
 
     # end
@@ -110,12 +114,12 @@ def create_graph_clicked():
         nVertices = int(ntr_vertices.get())
         window1.destroy()
 
-    btn_end = Button(window1, text = "  DONE! ", fg = 'green', font=("Helvetica", 13, "bold"), padx=3, pady=3, activebackground='grey', command=get_vertices_number)
+    btn_end = Button(window1, text = "  DONE! ", fg = 'black', font=("Helvetica", 13, "bold"), padx=3, pady=3, activebackground='grey', command=get_vertices_number)
     btn_end.place(relx = 0.8, rely=0.8, anchor=W)
 
-    window1.title('Fleury Algorithm')
+    window1.title('Create your own graph')
     window1.geometry("700x600+10+10")
-    window1.configure(bg='black')
+    window1.configure(bg='white')
     window1.mainloop()
 
 
@@ -130,29 +134,30 @@ btn_load_graph.place(relx = 0.75, rely=0.67, anchor=W)
 
 window.title('Fleury Algorithm')              # title of the window
 window.geometry("700x600+10+10")              # width * height + XPOS + YPOS
-window.configure(bg='black')
+window.configure(bg='white')
 window.mainloop()  
 
 # -------------------------------------------------------------------------------------------------
 
-# tworzenie plotna
+# Canvas creation
 plt.ion()
 fig = plt.figure()
 plot = fig.add_subplot()
 
 
-# GRAFICZNIE FLEURY ---------------------------------------------------------------------------------
-# rozpatrywana krawedz grafu jest zielona
+# GRAPHICAL REPRESENTATION OF THE FLEURY ALGORITHM ---------------------------------------------------------------------------------
+# the edge of the graph under consideration is green
 def graphic_consideredEdge(G, u, v, pos, colorr):
     if G.has_edge(u,v):
         plt.clf()
         G.remove_edge(u,v)
-        G.add_edge(u, v, color=colorr)
+        G.add_edge(u, v, color=colorr) # edge with color
         
         edges = G.edges()
         colors = [c for (u,v,c) in g_graphic.edges(data='color')]
         
-        plt.clf() # nowe
+        # re-drawing the figure
+        plt.clf()  
         nx.draw(G, pos=pos, edge_color = colors, node_size=800, with_labels=True)
         fig.canvas.draw()
         
@@ -160,7 +165,7 @@ def graphic_consideredEdge(G, u, v, pos, colorr):
         fig.canvas.flush_events()
         keyboard.wait('space')
         
-# usuwanie z grafu (plot) krawedzi
+# removing edge from the graph (plot)
 def graphic_removeEdge(G, u, v, pos):
     if G.has_edge(u,v):
         G.remove_edge(u,v)
@@ -175,9 +180,11 @@ def graphic_removeEdge(G, u, v, pos):
         keyboard.wait('space')
 
 # -------------------------------------------------------------------------------------------------
-    
+
+# FLEURY ALGORITHM --------------------------------------------------------------------------------
 class Graph:
   
+    # constructor
     def __init__(self,vertices):
         self.V= vertices #No. of vertices
         self.graph = defaultdict(list) # default dictionary to store graph
@@ -185,10 +192,10 @@ class Graph:
   
     # function to add an edge to graph
     def addEdge(self,u,v, isDirected):
-        if not isDirected:
+        if not isDirected:          # if graph NOT directed
             self.graph[u].append(v)
             self.graph[v].append(u)
-        else:
+        else:                       # else
             self.graph[u].append(v)
  
     # This function removes edge u-v from graph   
@@ -219,21 +226,21 @@ class Graph:
     def isValidNextEdge(self, u, v, graphic_graph, pos, isDirected):
         # The edge u-v is valid in one of the following two cases:
   
-          #  1) If v is the only adjacent vertex of u
+        #  1) If v is the only adjacent vertex of u
         if len(self.graph[u]) == 1:
             graphic_consideredEdge(graphic_graph, u, v, pos, 'green')
             return True
         else:
-            '''
-             2) If there are multiple adjacents, then u-v is not a bridge
-                 Do following steps to check if u-v is a bridge
-  
-            2.a) count of vertices reachable from u'''   
+            # 2) If there are multiple adjacents, then u-v is not a bridge
+            
+               
+            # Steps to check if u-v is a bridge:
+            # 2.a) count of vertices reachable from u  
             visited =[False]*(self.V)
             count1 = self.DFSCount(u, visited) 
  
-            '''2.b) Remove edge (u, v) and after removing the edge, count
-                vertices reachable from u'''
+            # 2.b) Remove edge (u, v) and after removing the edge, 
+            #      count vertices reachable from u
             self.rmvEdge(u, v, isDirected)
             
             visited =[False]*(self.V)
@@ -261,18 +268,17 @@ class Graph:
             #If edge u-v is not removed and it's a a valid next edge
             if self.isValidNextEdge(u, v, graphic_graph, pos, isDirected):
                 finalList.append("%d-%d" %(u,v))
-                #print("%d-%d " %(u,v))
                 graphic_removeEdge(graphic_graph, u, v, pos)
                 self.rmvEdge(u, v, isDirected)
                 self.printEulerUtil(v, graphic_graph, pos)
  
  
      
-    '''The main function that print Eulerian Trail. It first finds an odd
-   degree vertex (if there is any) and then calls printEulerUtil()
-   to print the path '''
+    # The main function that print Eulerian Trail. 
+    # It first finds an odd degree vertex (if there is any) 
+    # and then calls printEulerUtil() to print the path/circle:
     def printEulerTour(self, graphic_graph, pos, isEulerian):
-        #Find a start vertex -  with odd degree if isnt eulerian
+        #Find a start vertex -  with odd degree if graph is semieulerian
         u = 0
         if not isEulerian:
             for i in range(self.V):
@@ -280,7 +286,6 @@ class Graph:
                     u = i
                     break
             # Print tour starting from odd vertex
-            #print ("\n")
             self.printEulerUtil(u, graphic_graph, pos)
         else:
             self.printEulerUtil(u, graphic_graph, pos)
@@ -288,25 +293,30 @@ class Graph:
 # ----------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    if loadingGraph == True: # jesli graf jest wczytywany
-        # przyklad z wczytywaniem z pliku tekstowego
+    #Creating a graph based on the menu selections
+    if loadingGraph == True:    # if graph has to be loaded
+        # open file and read lines
         with open(filename) as f:
             lines = f.readlines()
-
+            
+        # read if graph is directed
         isDirected = True if lines[0].split()[0] == '->' else False
+        # read number of vertices
         nVertices = int(lines[1])
         g1 = Graph(nVertices)
 
         if isDirected:
-            g_graphic = nx.MultiDiGraph() # dimultigraf, bo może miec krawedzie wielokrotne/petle
+            g_graphic = nx.MultiDiGraph()   # dimultigraph, because it can have multiple edges/petals
         else:
-            g_graphic = nx.MultiGraph() # multigraf, bo może miec krawedzie wielokrotne/petle
+            g_graphic = nx.MultiGraph()     # Multigraph, because it can have multiple edges/petals
             
+        # read edges
         for i in range(2, len(lines)):
             line = lines[i].split()
             g1.addEdge(int(line[0]), int(line[1]), isDirected)
             g_graphic.add_edge(int(line[0]), int(line[1]), color='black')
-    else: # jesli graf jest tworzony
+            
+    else:                       # if graph has to be created
         g1 = Graph(nVertices)
         
         if isDirected:
@@ -319,60 +329,50 @@ if __name__ == "__main__":
             g_graphic.add_edge(edges_list[i][0], edges_list[i][1], color='black')
 
 
+    # checking if graph is eulerian/semieulerian
     isEulerian = nx.is_eulerian(g_graphic)
     isSemiEulerian = nx.is_semieulerian(g_graphic)
 
-
     if isEulerian or isSemiEulerian:
+        # drawing graph
         pos = nx.spring_layout(g_graphic)
         plot = nx.draw(g_graphic, pos=pos, node_size=800, with_labels=True)
         plt.show()
         # setting labels
         plt.title("Fleury working...")
 
+        # starting the algorithm
         g1.printEulerTour(g_graphic, pos, isEulerian)
         
+        
+        
+        # result window
         end_window = Tk()
         
-        lbl_end = Label(end_window, text = "Result", fg='yellow', font=("Helvetica", 22, "bold"), bg='black')
+        lbl_end = Label(end_window, text = "Result", fg='#2B45EC', font=("Helvetica", 22, "bold"), bg='white')
         lbl_end.place(relx=0.5, rely=0.32, anchor=CENTER)
         
-        txt = Text(end_window, width=50, height=5, font=("Helvetica", 18))
+        txt = Text(end_window, width=50, height=5, bd=4, font=("Helvetica", 18))
         txt.place(relx=0.5, rely=0.5, anchor=CENTER)
         for edge in finalList:
             txt.insert(END, edge + "   ")
         
-        end_window.title('Fleury Algorithm')              # title of the window
-        end_window.geometry("700x600+10+10")              # width * height + XPOS + YPOS
-        end_window.configure(bg='black')
+        end_window.title('Results')                     # title of the window
+        end_window.geometry("700x600+10+10")            # width * height + XPOS + YPOS
+        end_window.configure(bg='white')
         end_window.mainloop()  
         
     else:
         plt.close(fig)
         
         euler_window = Tk()
-        lbl_euler = Label(euler_window, text="Graph is not eulerian or semieulerian", fg='yellow', font=("Helvetica", 22, "bold"), bg='black')
+        lbl_euler = Label(euler_window, text="Graph is not eulerian or semieulerian", fg='#2B45EC', font=("Helvetica", 22, "bold"), bg='white')
         lbl_euler.place(relx=0.5, rely=0.5, anchor=CENTER)
         
-        euler_window.title('Fleury Algorithm')              # title of the window
-        euler_window.geometry("700x200+10+10")              # width * height + XPOS + YPOS
-        euler_window.configure(bg='black')
+        euler_window.title('Error!')                      # title of the window
+        euler_window.geometry("700x200+10+10")            # width * height + XPOS + YPOS
+        euler_window.configure(bg='white')
         euler_window.mainloop() 
         
 
-# ZROBIONE :)
-# 1. Sprawdzanie czy wprowadzony graf jest semieulerowski/eulerowski.
-# 2. Algorytm przyjmuje krawedzie wielokrotne i petle.
-# 3. Graficznie graf wyswietla petle.
-# 4. Chyba Algorytm przyjmuje tez grafy skierowane w zaleznosci od tego czy pliki ich zawieraja '-' lub '->'
 
-
-# NIEZROBIONE :(
-# 1. Graficznie graf naklada na siebie multikrawedzie zamiast pokazywac je np obok siebie. 
-
-'''
-Jeżeli wszystkie wierzchołki grafu nieskierowanego mają stopień parzysty, 
-a graf jest spójny, to znaczy, że da się skonstruować zamkniętą ścieżkę Eulera nazywaną cyklem Eulera. 
-Jeżeli najwyżej dwa wierzchołki mają nieparzysty stopień, to możliwe jest zbudowanie tylko takiej ścieżki Eulera, 
-która nie jest zamknięta.
-'''
